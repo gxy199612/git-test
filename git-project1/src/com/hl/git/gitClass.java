70a25035604f1783a975b3eeaca5def04bfb1529
gxy <gxy@qq.com>
2021-02-24 21:17:21 +0800

    将项目进行提交

---
+++ git-project1/src/com/hl/git/gitClass.java
@@ -0,0 +1,11 @@
+package com.hl.git;
+
+public class gitClass {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
